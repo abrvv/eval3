b7157c8b26f81f645335cee728a87014dbb20d6b
abrvv <wwcrmnnn@gmail.com>
2026-03-13 09:43:42 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4};
+  float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};
+
+  return 0;
+}
