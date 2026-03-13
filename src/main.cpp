806ac53a3ac4cae97bb3bf868729978bf4da8b3d
abrvv <wwcrmnnn@gmail.com>
2026-03-13 10:22:35 +0000

    4e commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void afficherBornes(const float puiss[], const float dist[], int taille){
-  for(int i; i<taille; i++){
-    cout << "Borne " << i+1 << " : " << puiss[i] << " kW à " << dist[i] << " km. \n";
+void afficherBornes(const float puiss[], const float dist[], int taille) {
+  for (int i = 0; i < taille; i++) {
+    cout << "Borne " << i + 1 << " : " << puiss[i] << " kW à " << dist[i]
+         << " km. \n";
   }
 }
 
-int main(){
+int indiceBorneLaPlusProche(const float dist[], int taille) {
+  float min = dist[0];
+  int indice = 0;
+  for (int i = 0; i < taille; i++) {
+    if (dist[i] < min) {
+      min = dist[i];
+      indice = i;
+    }
+  }
+  return indice;
+}
+
+int main() {
   float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4};
   float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};
 
-  afficherBornes(puissances,distances,5);
-  
+  afficherBornes(puissances, distances, 5);
+  cout << "La borne la plus proche est la borne :"
+       << indiceBorneLaPlusProche(distances, 5)+1 << "\n";
+
   return 0;
 }
