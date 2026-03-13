f133a5cea6e90829f888a128292bc087114e36c3
abrvv <wwcrmnnn@gmail.com>
2026-03-13 09:53:52 +0000

    3e commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 using namespace std;
 
+void afficherBornes(const float puiss[], const float dist[], int taille){
+  for(int i; i<taille; i++){
+    cout << "Borne " << i+1 << " : " << puiss[i] << " kW à " << dist[i] << " km. \n";
+  }
+}
+
 int main(){
   float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4};
   float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};
 
-  for(int i; i<5; i++){
-    cout << "Borne " << i+1 << " : " << puissances[i] << " kW à " << distances[i] << " km. \n";
-  }
+  afficherBornes(puissances,distances,5);
   
   return 0;
 }
