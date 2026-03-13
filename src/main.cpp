8fcde74283622c8638d5b75f3b7441144a14f35d
abrvv <wwcrmnnn@gmail.com>
2026-03-13 11:29:27 +0000

    fin

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void afficherBornes(const float puiss[], const float dist[], int taille) {
+struct Borne{
+  float puissance;
+  float distance;
+};
+
+void afficherBornes(const Borne borne[], int taille) {
   for (int i = 0; i < taille; i++) {
-    cout << "Borne " << i + 1 << " : " << puiss[i] << " kW à " << dist[i]
+    cout << "Borne " << i + 1 << " : " << borne[i].puissance << " kW à " << borne[i].distance
          << " km. \n";
   }
 }
 
-int indiceBorneLaPlusProche(const float dist[], int taille) {
-  float min = dist[0];
+int indiceBorneLaPlusProche(const Borne borne[], int taille) {
+  float min = borne[0].distance;
   int indice = 0;
   for (int i = 0; i < taille; i++) {
-    if (dist[i] < min) {
-      min = dist[i];
+    if (borne[i].distance < min) {
+      min = borne[i].distance;
       indice = i;
     }
   }
@@ -24,18 +29,22 @@ float calculerTempsRecharge(float capacite, float puissance){
   return (capacite/puissance)*60;
 }
 
-void modifierBorne(float dist[], int indiceBorne, float nouvelleDist){
+void modifierBorne(Borne borne[], int indiceBorne, float nouvelleDist){
   
-  dist[indiceBorne]=nouvelleDist;
+  borne[indiceBorne].distance=nouvelleDist;
 }
 
+
+
 int main() {
+  //Déclaration des bornes
+  Borne bornes[5] = {{50.0,12.5}, {22.0,35.0}, {11.0,8.0}, {150.0,60.0}, {7.4,2.3}};
+  
+  //Variables utilisateur
   int borne = 0;
   float capacite = 0.0;
   int newDist=0;
   int choix=0;
-  float puissances[5] = {50.0, 22.0, 11.0, 150.0, 7.4};
-  float distances[5] = {12.5, 35.0, 8.0, 60.0, 2.3};
 
   do{
     cout << "\n1. Afficher toutes les bornes\n";
@@ -47,21 +56,21 @@ int main() {
     cout << " Quel est votre choix ?"; cin >> choix;
 
     switch(choix){
-      case 1: afficherBornes(puissances, distances, 5);
+      case 1: afficherBornes(bornes, 5);
       break;
       case 2:
         cout << "La borne la plus proche est la borne :"
-           << indiceBorneLaPlusProche(distances, 5)+1 << "\n";
+           << indiceBorneLaPlusProche(bornes, 5)+1 << "\n";
       break;
       case 3:
         cout << "Quelle borne souhaitez vous utiliser ? "; cin >> borne;
         cout << "Quelle est la capacité du véhicule ? "; cin >> capacite;
-        cout << "Le temps de recharge est estimé à : "<< calculerTempsRecharge(capacite,puissances[borne-          1]) << " min. \n";
+        cout << "Le temps de recharge est estimé à : "<< calculerTempsRecharge(capacite,bornes[borne-          1].puissance) << " min. \n";
       break;
       case 4:
         cout << "Quelle borne souhaitez vous modifier la distance ? ";cin >> borne;
         cout << "Quelle distance ?"; cin >> newDist;
-        modifierBorne(distances, borne-1, newDist);
+        modifierBorne(bornes, borne-1, newDist);
         cout << "Changement effectué \n";
       break;
       case 5: break;
